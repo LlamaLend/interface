c3790e5f31781c368e3d62217da5d367355ea6cd
mintdart <96025197+mintdart@users.noreply.github.com>
2022-10-22 06:44:09 -0700

    add headers

--- next.config.js
+++ next.config.js
@@ -5,6 +5,27 @@ const nextConfig = {
   images: {
     domains: ['cloudflare-ipfs.com', 'nft.llamalend.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '\*',
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, content-type, Authorization',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
