533351688c60b08af37a00e75fb18f7ab0f5033a
mintdart <96025197+mintdart@users.noreply.github.com>
2022-10-26 21:30:39 +0200

    add emergency shutdown page

---
+++ pages/shutdown.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { useAccount, useNetwork } from 'wagmi'
+import BeatLoader from '~/components/BeatLoader'
+import Layout from '~/components/Layout'
+import { useShutdownAllPools } from '~/queries/admin/useEmergencyShutdown'
+
+const ManagePools: NextPage = () => {
+	const { isConnected } = useAccount()
+	const { chain } = useNetwork()
+
+	const { write: emergencyShutdown, isLoading: approvingShutdown } = useShutdownAllPools()
+
+	return (
+		<>
+			<Head>
+				<title>Shutdown Pools - LlamaLend</title>
+			</Head>
+
+			<Layout>
+				<button
+					className="mx-auto mt-12 min-h-[2.5rem] w-[7.5rem] rounded-lg bg-[#243b55] p-2 text-center text-sm text-white disabled:cursor-not-allowed"
+					disabled={!emergencyShutdown || chain?.unsupported || !isConnected}
+				>
+					{approvingShutdown ? <BeatLoader /> : 'Shutdown'}
+				</button>
+			</Layout>
+		</>
+	)
+}
+
+export default ManagePools
--- queries/admin/useEmergencyShutdown.tsx
+++ queries/admin/useEmergencyShutdown.tsx
@@ -5,6 +5,7 @@ import toast from 'react-hot-toast'
 import { txConfirming, txError, txSuccess } from '~/components/TxToast'
 import { useTxContext } from '~/contexts'
 import { chainConfig } from '~/lib/constants'
+import { useGetAllPools } from '../useGetAllPools'
 
 interface IShutdownBorrows {
 	userAddress: string
@@ -31,6 +32,63 @@ export default function useEmergencyShutdown({ userAddress, chainId, poolAddress
 		functionName: 'setMaxPrice',
 		args: '0',
 		enabled: chain?.id === chainId && address?.toLowerCase() === userAddress?.toLowerCase()
+	})
+
+	const contractWrite = useContractWrite({
+		...contractConfig,
+		onSuccess: (data) => {
+			txContext.hash!.current = data.hash
+			txContext.dialog?.toggle()
+
+			txConfirmingId.current = txConfirming({ txHash: data.hash, blockExplorer: config.blockExplorer })
+		}
+	})
+
+	const waitForTransaction = useWaitForTransaction({
+		hash: contractWrite.data?.hash,
+		onSettled: (data) => {
+			toast.dismiss(txConfirmingId.current)
+
+			if (data?.status === 1) {
+				txSuccess({
+					txHash: contractWrite.data?.hash ?? '',
+					blockExplorer: config.blockExplorer,
+					content: 'Transaction Success'
+				})
+			} else {
+				txError({ txHash: contractWrite.data?.hash ?? '', blockExplorer: config.blockExplorer })
+			}
+
+			queryClient.invalidateQueries()
+		}
+	})
+
+	return {
+		...contractWrite,
+		waitForTransaction
+	}
+}
+
+export function useShutdownAllPools() {
+	const { isConnected } = useAccount()
+	const { chain } = useNetwork()
+
+	const txContext = useTxContext()
+
+	const queryClient = useQueryClient()
+
+	const config = chainConfig(chain?.id)
+
+	const txConfirmingId = useRef<string>()
+
+	const { data } = useGetAllPools({ chainId: chain?.id })
+
+	const { config: contractConfig } = usePrepareContractWrite({
+		addressOrName: config.factoryAddress,
+		contractInterface: config.factoryABI,
+		functionName: 'emergencyShutdown',
+		args: [data?.map((pool) => pool.address)],
+		enabled: isConnected && !chain?.unsupported && data ? true : false
 	})
 
 	const contractWrite = useContractWrite({
