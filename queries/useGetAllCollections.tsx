527df21fe4e6c3280e6baa51b34a3607fdb0bd8f
0xngmi <0xngmi@protonmail.com>
2022-11-07 15:58:49 +0000

    Revert "filter collections with no borrowable loans"
    This reverts commit 34e9b97f437f5aca8adcc0c90251c9ae2b4bd658.

--- queries/useGetAllCollections.tsx
+++ queries/useGetAllCollections.tsx
@@ -1,66 +1,49 @@
 import { useQuery } from '@tanstack/react-query'
+import { ethers } from 'ethers'
 import { getAddress } from 'ethers/lib/utils'
-import { chainConfig } from '~/lib/constants'
+import { chainConfig, ERC721_ABI } from '~/lib/constants'
 import { getAllpools } from './useGetAllPools'
 import verifiedCollections from '~/lib/collections'
 import type { ICollection } from '~/types'
-import { getPool } from './useGetPoolData'
 
-const getPoolInfo = async ({ poolAddress, chainId }: { poolAddress: string; chainId?: number | null }) => {
+const getCollectionName = async ({ address, chainId }: { address: string; chainId?: number | null }) => {
 	const config = chainConfig(chainId)
 
-	const contractArgs = poolAddress
-		? {
-				address: poolAddress,
-				abi: config.poolABI,
-				provider: config.chainProvider
-		  }
-		: null
+	const nftContract = new ethers.Contract(address, ERC721_ABI, config.chainProvider)
 
-	return await getPool({
-		contractArgs,
-		chainId,
-		quoteApi: config.quoteApi,
-		isTestnet: config.isTestnet,
-		graphEndpoint: config.subgraphUrl
-	})
+	return await nftContract.name()
 }
 
 export async function getAllCollections({ chainId }: { chainId?: number | null }) {
 	const pools = await getAllpools({ chainId })
-
-	const poolsWithAddlData = (
-		await Promise.all(pools.map((pool) => getPoolInfo({ poolAddress: pool.address, chainId })))
-	).filter((pool) => !!pool && Number(pool.maxNftsToBorrow) > 0)
-
 	const collections = new Set<string>()
 
-	poolsWithAddlData.forEach((pool) => {
-		if (pool) {
-			collections.add(getAddress(pool?.nftContract))
-		}
+	pools.forEach((pool) => {
+		collections.add(getAddress(pool.nftContract))
 	})
+
+	const collectionNames = await Promise.all(
+		Array.from(collections).map((address) => getCollectionName({ address, chainId }))
+	)
 
 	const verified: Array<ICollection> = []
 
 	const notVerified: Array<ICollection> = []
 
-	Array.from(collections).forEach((address) => {
+	Array.from(collections).forEach((address, index) => {
 		const verifiedCollectionIndex = verifiedCollections[chainId || 1].findIndex(
 			(x) => x.address.toLowerCase() === address.toLowerCase()
 		)
 
-		const name = poolsWithAddlData.find((pool) => pool?.nftContract?.toLowerCase() === address.toLowerCase())?.nftName
-
 		if (verifiedCollectionIndex + 1) {
 			verified.push({
 				address,
-				name: name,
+				name: collectionNames[index],
 				imgUrl: verifiedCollections[chainId || 1][verifiedCollectionIndex].imgUrl,
 				sortIndex: verifiedCollectionIndex + 1
 			})
 		} else {
-			notVerified.push({ address, name: name, imgUrl: '', sortIndex: -1 })
+			notVerified.push({ address, name: collectionNames[index], imgUrl: '', sortIndex: -1 })
 		}
 	})
 
