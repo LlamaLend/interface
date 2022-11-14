bfd541dd9d167b9c348b5298160bde7b8d9d5844
mintdart <96025197+mintdart@users.noreply.github.com>
2022-11-14 08:29:04 +0100

    hydrate collections query on client

--- containers/BorrowCollectionsContainer.tsx
+++ containers/BorrowCollectionsContainer.tsx
@@ -2,15 +2,15 @@ import Head from 'next/head'
 import { BorrowCollectionItem } from '~/components/GridItem/Collection'
 import GridWrapper from '~/components/GridWrapper'
 import Layout from '~/components/Layout'
-import type { ICollection } from '~/types'
+import { useGetAllCollections } from '~/queries/useGetAllCollections'
 
 interface IPoolsContainerProps {
 	chainId: number
 	chainName: string
-	collections: Array<ICollection>
 }
 
-const BorrowCollectionsContainer = ({ chainId, chainName, collections }: IPoolsContainerProps) => {
+const BorrowCollectionsContainer = ({ chainId, chainName }: IPoolsContainerProps) => {
+	const { data: collections } = useGetAllCollections({ chainId })
 	return (
 		<>
 			<Head>
@@ -20,11 +20,11 @@ const BorrowCollectionsContainer = ({ chainId, chainName, collections }: IPoolsC
 			<Layout>
 				{!chainId || !chainName ? (
 					<p className="fallback-text">Network not supported. No pools on {chainName || 'this network'}.</p>
-				) : collections.length === 0 ? (
+				) : collections?.length === 0 ? (
 					<p className="fallback-text">There are no collections on {chainName || 'this'} network.</p>
 				) : (
 					<GridWrapper className="mx-0 mt-8 mb-auto !place-content-around sm:my-9">
-						{collections.map((item) => (
+						{collections?.map((item) => (
 							<BorrowCollectionItem key={item.address} data={item} chainName={chainName} />
 						))}
 					</GridWrapper>
