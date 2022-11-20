9419637ba5de41f059458d1ebe2476c01d018826
mintdart <96025197+mintdart@users.noreply.github.com>
2022-11-20 14:39:56 +0100

    cleanup

--- components/Form/Slider.tsx
+++ components/Form/Slider.tsx
@@ -29,7 +29,7 @@ export const Slider = ({
 		<label className="flex flex-col gap-2">
 			<span className="-mb-1 flex w-full justify-between text-xs text-[#3E424E]">
 				<span>{min}</span>
-				<span>{max}</span>
+				{min !== max && <span>{max}</span>}
 			</span>
 			<RadixSlider.Root
 				className="relative flex h-[20px] w-full touch-none select-none items-center"
@@ -38,7 +38,7 @@ export const Slider = ({
 				max={max}
 				step={step}
 				minStepsBetweenThumbs={step}
-				disabled={disabled}
+				disabled={disabled || min === max}
 				value={value}
 				onValueChange={(newValue) => setValue(newValue)}
 				onValueCommit={onValueCommit}
@@ -49,9 +49,11 @@ export const Slider = ({
 				<RadixSlider.Thumb className="relative block h-3 w-3 rounded-full border-2 border-white bg-[#3046FB]">
 					<span className="absolute -top-6 -left-1 whitespace-nowrap bg-primary px-[2px] text-xs text-white">{`${value[0]} ${symbol}`}</span>
 				</RadixSlider.Thumb>
-				<RadixSlider.Thumb className="relative block h-3 w-3 rounded-full border-2 border-white bg-[#3046FB]">
-					<span className="absolute -top-6 -right-1 whitespace-nowrap bg-primary px-[2px] text-xs text-white">{`${value[1]} ${symbol}`}</span>
-				</RadixSlider.Thumb>
+				{min !== max && (
+					<RadixSlider.Thumb className="relative block h-3 w-3 rounded-full border-2 border-white bg-[#3046FB]">
+						<span className="absolute -top-6 -right-1 whitespace-nowrap bg-primary px-[2px] text-xs text-white">{`${value[1]} ${symbol}`}</span>
+					</RadixSlider.Thumb>
+				)}
 			</RadixSlider.Root>
 			<span className="text-sm text-[#D4D4D8]">{label}</span>
 		</label>
