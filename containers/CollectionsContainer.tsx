2133181e35c43817b15f8f4d005b4412e6e942ea
shibacrypt <>
2022-12-03 20:29:42 +0800

    Add grid/list toggle button

--- containers/CollectionsContainer.tsx
+++ containers/CollectionsContainer.tsx
@@ -3,19 +3,61 @@ import { useState } from 'react'
 import { BorrowCollectionItemCard, BorrowCollectionItemList } from '~/components/Collection'
 import Layout from '~/components/Layout'
 import { useGetAllCollections } from '~/queries/useGetAllCollections'
+import { ICollection } from '~/types'
 
 enum ViewType {
 	Card,
 	List,
 }
+
 interface ICollectionContainerProps {
 	chainId: number
 	chainName: string
 }
 
+const renderCollectionItem = ({ chainId, chainName }: ICollectionContainerProps, collections: ICollection[] | undefined, viewType: ViewType ) => (
+	viewType === ViewType.Card ? (
+		<ul className="mx-0 mt-8 mb-auto grid grid-cols-[repeat(auto-fit,minmax(240px,260px))] place-content-around gap-8 sm:my-9 2xl:place-content-between">
+			{collections?.map((item) => (
+				<BorrowCollectionItemCard key={item.address} data={item} chainName={chainName} />
+			))}
+		</ul>
+	) : (
+		<ul className="grid grid-rows-1">
+			{collections?.map((item) => (
+				<BorrowCollectionItemList key={item.address} data={item} chainId={chainId} chainName={chainName} />
+			))}
+		</ul>
+	)
+)
+
+const ViewTypeSwitch = ({ viewType, onClick } : { viewType: ViewType, onClick: () => void }) => {
+	return (
+		<div className="flex items-center justify-end">
+			<div className="inline-flex pb-2" role="group" onClick={onClick}>
+				<button
+					type="button"
+					className={`rounded-l px-2 py-1 border-2 border-white font-medium text-xs ${viewType === ViewType.Card && "bg-white text-black"}`}
+				>
+					Grid
+				</button>
+				<button 
+					type="button"
+					className={`rounded-r px-2 py-1 border-2 border-white font-medium text-xs ${viewType === ViewType.List && "bg-white text-black"}`}
+				>
+					List
+				</button>
+			</div>
+		</div>
+	)
+}
+
 const CollectionsContainer = ({ chainId, chainName }: ICollectionContainerProps) => {
-	const [viewType, setViewType] = useState<ViewType>(ViewType.List)
+	const [viewType, setViewType] = useState<ViewType>(ViewType.Card)
 	const { data: collections } = useGetAllCollections({ chainId, skipOracle: true })
+	const toggleViewType = () => {
+		viewType === ViewType.Card ? setViewType(ViewType.List) : setViewType(ViewType.Card)
+	}
 	return (
 		<>
 			<Head>
@@ -28,19 +70,10 @@ const CollectionsContainer = ({ chainId, chainName }: ICollectionContainerProps)
 				) : collections?.length === 0 ? (
 					<p className="fallback-text">There are no collections on {chainName || 'this'} network.</p>
 				) : (
-					viewType === ViewType.Card ? (
-						<ul className="mx-0 mt-8 mb-auto grid grid-cols-[repeat(auto-fit,minmax(240px,260px))] place-content-around gap-8 sm:my-9 2xl:place-content-between">
-							{collections?.map((item) => (
-								<BorrowCollectionItemCard key={item.address} data={item} chainName={chainName} />
-							))}
-						</ul>
-					) : (
-						<ul className="grid grid-rows-1">
-							{collections?.map((item) => (
-								<BorrowCollectionItemList key={item.address} data={item} chainId={chainId} chainName={chainName} />
-							))}
-					</ul>
-					)
+					<>
+						<ViewTypeSwitch viewType={viewType} onClick={toggleViewType} />
+						{renderCollectionItem({chainName, chainId}, collections, viewType)}
+					</>
 				)}
 			</Layout>
 		</>
